8af7a6213aa2f19136da4d9b49e587fe5234d193
davidka7 <david.perekopskiy@go.shoreline.edu>
2020-01-29 13:48:28 -0800

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,46 @@
 # code here!
+require "pry"
+class School
+    
+    def initialize(name)
+@name=name
+@hash={}
+    end
+
+    def roster 
+        @hash
+    end
+
+
+def add_student(key, grade)
+    #if @hash.include?(key) == false
+       # @hash[value]=[]
+    #end
+     #@hash[grade] == false
+       # @hash[grade] = [key]
+       if @hash.include?(grade) == false
+        @hash[grade] = [key]
+       else @hash[grade] << key
+
+
+
+    #if @hash[grade]
+   #     @hash[grade] = [key]
+    end
+#binding.pry
+end
+def grade(grade)
+@hash[grade]
+end
+
+ def sort
+#@hash.sort_by{|k,v|k
+#binding.pry
+@hash.each_key {|grade| @hash[grade]=@hash[grade].sort 
+
+
+}
+
+#}
+ end
+end
