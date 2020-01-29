6869f2348412aa7e586d512966195f55a544c0fa
dianakw8591 <dianakw@gmail.com>
2020-01-28 17:29:36 -0800

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(student, grade)
+        if @roster[grade].nil? 
+            @roster[grade] = []
+        end
+        @roster[grade] << student
+    end
+        
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.reduce({}) do |memo, (grade, students)|
+            memo[grade] = students.sort
+            memo
+        end
+    end
+end
