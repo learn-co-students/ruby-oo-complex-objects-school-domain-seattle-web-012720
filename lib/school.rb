e3b03d086c098ce9ff712deded8710a43efe88fd
nk2303 <ngankhong03@gmail.com>
2020-01-29 17:51:52 -0800

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+require 'pry'
+
+class School
+    def initialize(roster)
+        @roster = Hash.new
+    end
+
+    attr_accessor :roster
+
+    def add_student(name, grade)
+        if !@roster[grade]
+            @roster[grade] = [name]
+        else
+            @roster[grade] << name
+        end
+        @roster
+    end
+
+    def grade(num)
+        @roster[num]
+    end
+
+    def sort
+        @roster.each { |k,v|
+            @roster[k] = v.sort
+        }
+        Hash[@roster.sort]
+    end
+end
+
+#1 => [a,b,c]
