93ad3b4d874184f9219fc5add2ada67c504cd789
GabedeSousa <gabriel.fidelis.sousa@gmail.com>
2021-04-28 16:55:34 -0700

    going over oop

--- lib/school.rb
+++ lib/school.rb
@@ -1,45 +1,39 @@
-# code here!
-class School
+class School 
+    attr_accessor :school_name, :roster
 
     def initialize(school_name)
         @school_name = school_name
         @roster = {}
     end
 
-    def roster
-        @roster
+    def add_student(student_name, key_number)
+        # this will create a key number pointedd to an empty array
+        #the ||= will make it possible to GET ALL students from that grade, even if it's not empty. a || a = b
+        roster[key_number] ||= []
+        roster[key_number] << student_name
     end
 
-    def add_student(name, grade)
-        @name = name
-        @grade = grade
-        if roster.include?(grade) == false
-            roster[grade] = []
-        end
-        @roster[grade] << name
-    end
-
-    def grade(grade)
-        @roster[grade]
+    def grade(student_grade)
+        roster[student_grade]
     end
 
     def sort
-        @roster.each do |grade, name|
-            @roster[grade] = name.sort
+        sorted = {}
+        roster.each do |grade, students|
+            sorted[grade] = students.sort
         end
+        sorted
     end
+
 
 end
 
-
-
-
-
-
-
-
-
-
+school = School.new('Coc')
+school.add_student('Gabriel', 10)
+school.add_student('Bibi', 11)
+school.add_student('Rafa', 12)
+school.add_student('Ana', 12)
+puts school.grade(12)
 
 
 
